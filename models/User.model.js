4cec694e01a5d7aa2f631d3aac6309559620af32
ganesh358 <ganeshmoremc@gmail.com>
2023-03-16 10:40:27 +0530

    back

--- models/User.model.js
+++ models/User.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     user_id : {type : Number},
     email : {type : String, required : true},
     password : {type : String, required : true},
-    age : {type : Number, required : true},
+    name : {type : String, required : true},
 })
 
 const UserModel = mongoose.model("user", userSchema)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,7 +9,7 @@ const userController = Router();
 
 
 userController.post("/signup", (req, res) => {
-    const {email, password, age} = req.body;
+    const {email, password,name} = req.body;
 
     bcrypt.hash(password, 5, async function(err, hash) {
         if(err){
@@ -18,7 +18,8 @@ userController.post("/signup", (req, res) => {
         const user = new UserModel({
             email,
             password : hash,
-            age
+            name
+
         })
         try{
             await user.save()
@@ -38,14 +39,15 @@ userController.post("/login", async (req, res) => {
     const hash = user.password
     bcrypt.compare(password, hash, function(err, result) {
         if(err){
-            res.send("Something went wrong, plz try again later")
+            res.json({massage:"Something went wrong, plz try again later"})
         }
         if(result){
             const token = jwt.sign({ userId : user._id }, process.env.JWT_SECRET);
             res.json({message : "Login successfull", token})
         }
         else{
-            res.send("Invalid credentials, plz signup if you haven't")
+           
+             res.send("Invalid credentials, plz signup if you haven't")
         }
     });
     
